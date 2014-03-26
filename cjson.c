7d88daea565184243bd9dc4bf93dbd36ffb830f1
Tyler Neylon <tylerneylon@gmail.com>
2014-03-25 21:44:03 -0700

    Add utf-8 functions and untested escaped_str func.

--- cjson.c
+++ cjson.c
@@ -19,6 +19,64 @@
 #define false 0
 
 
+// Globals.
+
+static char *encoded_chars = "bfnrt\"\\";
+static char *decoded_chars = "\b\f\n\r\t\"\\";
+
+
+// UTF-8 functions.
+//
+// These are from:
+// https://gist.github.com/tylerneylon/9773800
+
+int decode_code_point(char **s) {
+  int k = **s ? __builtin_clz(~(**s << 24)) : 0; // Count # of leading 1 bits.
+  int mask = (1 << (8 - k)) - 1;  // All 1 bits with k leading 0's.
+  int value = **s & mask;
+  for (++(*s); k > 0 && **s; --k, ++(*s)) {  // Note that k = #total bytes, or 0.
+    value <<= 6;
+    value += (**s & 0x3F);
+  }
+  return value;
+}
+
+// Assumes that code is <= 0x10FFFF.
+// Ensures that nothing will be written at or beyond end.
+void encode_code_point(char **s, char *end, int code) {
+  static char val[4];
+  int lead_byte_max = 0x7F;
+  int val_index = 0;
+  while (code > lead_byte_max) {
+    val[val_index++] = (code & 0x3F) | 0x80;
+    code >>= 6;
+    lead_byte_max >>= (val_index == 1 ? 2 : 1);
+  }
+  val[val_index++] = (code & lead_byte_max) | (~lead_byte_max << 1);
+  while (val_index-- && *s < end) {
+    **s = val[val_index];
+    (*s)++;
+  }
+}
+
+// Returns 0 if no split was needed.
+int split_into_surrogates(int code, int *surr1, int *surr2) {
+  if (code <= 0xFFFF) return 0;
+  *surr2 = code & 0x3FF;                      // Save the low 10 bits.
+  code >>= 10;                                // Drop the low 10 bits.
+  *surr1 = code & 0x3F;                       // Save the next 6 bits.
+  *surr1 |= ((code & (0x1F << 6)) - 1) << 6;  // Prepend the last 5 bits less 1.
+  return 1;
+}
+
+// Returns 0 if no join was needed.
+int join_from_surrogates(int surr1, int surr2, int *code) {
+  if (surr1 < 0xD800 || surr1 > 0xDFFF) return 0;
+  *code = ((surr1 & 0x3FF + 0x40) << 10) + (surr2 & 0x3FF);
+  return 1;
+}
+
+
 // Internal functions.
 
 int str_hash(void *str_void_ptr) {
@@ -50,10 +108,8 @@ int str_eq(void *str_void_ptr1, void *str_void_ptr2) {
   c = *input++; \
   if (c == '\\') { \
     c = *input++; \
-    static char *esc_input = "bfnrt"; \
-    static char *esc_output = "\b\f\n\r\t"; \
-    char *esc_ptr = strchr(esc_input, c); \
-    if (esc_ptr) c = esc_output[esc_ptr - esc_input]; \
+    char *esc_ptr = strchr(encoded_chars, c); \
+    if (esc_ptr) c = decoded_chars[esc_ptr - encoded_chars]; \
   } \
   *(char *)CArrayNewElement(char_array) = c;
 
@@ -290,7 +346,7 @@ char *parse_value(Item *item, char *input, char *input_start) {
 }
 
 // Expects the input array to have elements of type char *.
-int array_printf(CArray array, const char *fmt, ...) {
+static int array_printf(CArray array, const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
   int return_value = vasprintf((char **)CArrayNewElement(array), fmt, args);
@@ -299,13 +355,53 @@ int array_printf(CArray array, const char *fmt, ...) {
 }
 
 // Expects the array to have elements of type char *. Caller owns the newly-allocated return val.
-char *array_join(CArray array) {
+static char *array_join(CArray array) {
   int total_len = 0;
   CArrayFor(char **, str_ptr, array) total_len += strlen(*str_ptr);
   char *str = malloc(total_len + 1);  // +1 for the final null.
   char *tail = str;
   CArrayFor(char **, str_ptr, array) tail = stpcpy(tail, *str_ptr);
   return str;
+}
+
+#define new_elt(arr) *(char *)CArrayNewElement(arr)
+
+#define add_u_escaped_pt(arr, pt) \
+  new_elt(arr) = '\\'; \
+  new_elt(arr) = 'u'; \
+  for (int i = 0; i < 4; ++i, pt >>= 4) hex_digits[3 - i] = hex[pt % 16]; \
+  for (int i = 0; i < 4; ++i) new_elt(arr) = hex_digits[i];
+
+// Caller must free the returned string.
+static char *escaped_str(char *s) {
+  CArray array = CArrayNew(4, sizeof(char));
+
+  // These are used in add_u_escaped_pt to encode non-ascii unicode characters.
+  static char *hex = "0123456789ABCDEF";
+  char hex_digits[4];
+
+  while (*s) {
+    int code_pt = decode_code_point(&s);
+    char *esc_ptr = strchr(decoded_chars, code_pt);
+    if (esc_ptr) {
+      new_elt(array) = '\\';
+      new_elt(array) = encoded_chars[esc_ptr - decoded_chars];
+    } else if (code_pt < 0x80) {
+      new_elt(array) = code_pt;
+    } else {
+      int surr1, surr2;
+      if (split_into_surrogates(code_pt, &surr1, &surr2)) {
+        add_u_escaped_pt(array, surr1);
+        add_u_escaped_pt(array, surr2);
+      } else {
+        add_u_escaped_pt(array, code_pt);
+      }
+    }
+  }
+  new_elt(array) = '\0';
+  char *escaped_s = array->elements;
+  CArrayFreeButLeaveElements(array);
+  return escaped_s;
 }
 
 void print_item(CArray array, Item item, char *indent, int be_terse) {
